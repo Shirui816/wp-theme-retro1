899c3908f8e9ec7d6578348a9b82973bfd2eafff
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2026-03-09 15:39:39 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div><footer id="colophon" style="text-align: center; padding: 40px 0; font-family: monospace; font-size: 0.8rem; color: #666;">
+        <p>
+            &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.
+			LICENSED UNDER <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en">CC-BY-NC-SA 4.0</a>
+            POWERED BY WORDPRESS. NO_JS_REQUIRED (MOSTLY).
+        </p>
+    </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
